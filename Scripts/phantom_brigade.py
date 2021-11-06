de49fff9df16ae89882295da3f51ae0998de45de
ichihito-ohi <ichihito.ohi@gmail.com>
2021-11-06 23:18:42 +0900

    update id format

--- Scripts/phantom_brigade.py
+++ Scripts/phantom_brigade.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 import pathlib
 import yaml
 
@@ -19,18 +20,21 @@ class Extractor:
             print(e, file=sys.stderr)
 
     # Make a line to write into csv with the defined format
-    def formCsvLine(self, path:pathlib.Path, text:str=None, subkey1:str='', subkey2:str='', subkey3:str=''):
+    def formCsvLine(self, path:pathlib.Path, text:str=None, subkey_list:List[Union[int, str]]=None):
         folder = str(path.relative_to(self.root_path).parent.as_posix())
         file = str(path.stem)
         key = "%s/%s" % (folder, file)
-        if subkey1 != '':
-            key += ":%s" % subkey1
-        if subkey2 != '':
-            key += ".%s" % subkey2
-        if subkey3 != '':
-            key += ".%s" % subkey3
+
+        i = 0
+        for subkey in subkey_list:
+            if i == 0:
+                key += ":%s" % subkey
+            else:
+                key += ".%s" % subkey
+            i += 1
+
         str_text = str(text).replace('"', '""')
-        line = "%s,%s,%s,%s,%s,%s,\"%s\"\n" % (folder, file, subkey1, subkey2, subkey3, key, str_text)
+        line = "%s,%s,%s,\"%s\"\n" % (folder, file, key, str_text)
         return line
 
     # Convert constructor in yaml
