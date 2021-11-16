99c65f84be118d79254e5140158c83aacf3192c2
ichihito-ohi <ichihito.ohi@gmail.com>
2021-11-17 01:12:47 +0900

    add script to overwrite yaml in Text folder

--- Scripts/phantom_brigade.py
+++ Scripts/phantom_brigade.py
@@ -19,6 +19,21 @@ class Extractor:
         except Exception as e:
             print(e, file=sys.stderr)
 
+    # Make a id with the defined format
+    def formId(self, path:pathlib.Path, subkey_list:List[Union[int, str]]=None):
+        folder = str(path.relative_to(self.root_path).parent.as_posix())
+        file = str(path.stem)
+        key = "%s/%s" % (folder, file)
+
+        i = 0
+        for subkey in subkey_list:
+            if i == 0:
+                key += ":%s" % subkey
+            else:
+                key += ".%s" % subkey
+            i += 1
+        return key
+
     # Make a line to write into csv with the defined format
     def formCsvLine(self, path:pathlib.Path, text:str=None, subkey_list:List[Union[int, str]]=None):
         folder = str(path.relative_to(self.root_path).parent.as_posix())
@@ -61,9 +76,19 @@ class Extractor:
         yaml.add_constructor('!UnitPresetEmbedded', self.construct)
         pass
 
+    # Represent null
+    def represent_null(self, dumper, data):
+        return dumper.represent_scalar(u'tag:yaml.org,2002:null', '')
+    
+    # Add representers to yaml dumper
+    def add_multi_representors(self):
+        yaml.add_representer(type(None), self.represent_null)
+        pass
+
     # Initiate Extractor
     def __init__(self, root_path:pathlib.Path):
         self.root_path = root_path
         self.ver = self.getVersion()
         self.add_multi_constructors()
+        self.add_multi_representors()
         pass
